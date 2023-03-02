467717811681b89c79f66c3284ceb153548f6778
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-03-02 22:34:50 +0530

    Added: Refactor code for check employee total wage

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -1,36 +1,41 @@
 package com.employeewage;
 
 public class EmployeeWageBuilder {
-    public static final int IS_FULL_TIME = 1;//constants
-    public static final int IS_PART_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NO_OF_WORKING_DAYS = 20;
-    public static final int NO_OF_WORKING_HOURS = 50;
-
-    public static void main(String[] args) {
-        //Variables
+    static final int IS_FULL_TIME = 1;
+    static final int EMP_RATE_PER_HOUR = 20;
+    static final int IS_PART_TIME = 2;
+    static final int NO_OF_WORKING_DAYS = 20;
+    static int employeeCheck(){
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+        return empCheck;
+    }
+    public int dailyWageOfEmployee() {
         int empHrs = 0;
         int totalHrs = 0;
+        int totalWage = 0;
         int totalWorkingDays = 0;
-        while (totalHrs <= NO_OF_WORKING_HOURS && totalWorkingDays < NO_OF_WORKING_DAYS) {
+        while (totalWorkingDays < NO_OF_WORKING_DAYS) {
             totalWorkingDays++;
-
-            int empCheck = (int) Math.floor((Math.random() * 10) % 3);
-            switch (empCheck) { //checking if part-time
+            switch (employeeCheck()) {
                 case IS_PART_TIME:
                     empHrs = 4;
                     break;
-                case IS_FULL_TIME: //checking if full time or not
+                case IS_FULL_TIME:
                     empHrs = 8;
                     break;
                 default:
                     empHrs = 0;
             }
-            totalHrs += empHrs;
-            System.out.println("Day#: " + totalWorkingDays + " " + "Emp hr: " + totalHrs);
+            totalHrs+=empHrs;
+            System.out.println("Day: " + totalWorkingDays + " " + "Emp hr: " + totalHrs);
         }
-        int totalWage = totalHrs * totalWorkingDays;
-        System.out.println("Total employee wage: " + totalWage);
+        totalWage = totalHrs * EMP_RATE_PER_HOUR;
+        return totalWage;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+        EmployeeWageBuilder employee = new EmployeeWageBuilder();
+        System.out.println("Total Wage: " + employee.dailyWageOfEmployee());
     }
 }
